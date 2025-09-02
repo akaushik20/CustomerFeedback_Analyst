c56a65621a067b91642d962c0fc49cf760ebde8d
Arpita Kaushik <arpita.k20@gmail.com>
2025-09-02 15:28:44 -0700

    commit

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-PROD_USAGE_FILE_PATH = os.path.join(BASE_DIR, 'data\product_usage.csv')
+#PROD_USAGE_FILE_PATH = os.path.join(BASE_DIR, 'data\product_usage.csv')
+PROD_USAGE_FILE_PATH = "https://raw.githubusercontent.com/akaushik20/CustomerFeedback_Analyst/refs/heads/main/data/product_usage.csv"
 FEEDBACK_FILE_PATH = os.path.join(BASE_DIR, 'data', 'feedback.jsonl')
 
 #THEME_EXTRACTION_PROMPT = """You are a theme extraction agent. 
